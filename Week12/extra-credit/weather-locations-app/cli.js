f7a1f86942990ce8efbed4a5f0175db5ce86cc5a
hasanain87 <AL_SABONCHI_HASANAIN@student.smc.edu>
2020-07-19 02:20:57 -0700

    add folers

--- Week12/extra-credit/weather-locations-app/cli.js
+++
@@ -1,8 +0,0 @@
-// complete the yargs command to ensure it meets the criteria outlined in the instructions
-// - needs a location command
-// - needs to include help support
-yargs.usage('$0: Usage <cmd> [options]').command({
-    command: '',
-    desc: '',
-    handler: (argv) => {},
-});
